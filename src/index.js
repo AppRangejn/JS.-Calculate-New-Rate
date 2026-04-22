0fd649d207e49bdc31aa5d42d8f2973e7e267931
Kiril <kiril0302ua@gmail.com>
2026-04-23 02:01:04 +0300

    Fix, more simply check in function

--- src/index.js
+++ src/index.js
@@ -1,57 +1,28 @@
 'use strict';
 
-const valueWinner = Number(prompt("Введіть рейтинг переможця:"));
-const valueOpponent = Number(prompt("Введіть рейтинг суперника:"));
-
-if(isNaN(valueWinner) || isNaN(valueOpponent) || valueWinner < 0 || valueOpponent < 0){
-    console.error("Помилка, дані введено некоректно(текст або число менше за нуль)");
-}
-else{
-    const newRating = calculateRating(valueWinner, valueOpponent);
-    console.log(`Новий рейтинг переможця: ${newRating}`);
-}
-
 /**
  * @param {number} ratingWinner 
  * @param {number} ratingOpponent 
  * @returns {number}
  */
 function calculateRating(ratingWinner, ratingOpponent){
-    if(isNaN(ratingWinner) || isNaN(ratingOpponent) || ratingWinner < 0 || ratingOpponent < 0){
-        return 0;
+    if(isNaN(ratingWinner + ratingOpponent) || ratingWinner < 0 || ratingOpponent < 0){
+        return NaN;
     }
 
-    let bonus = 0;
-    let result = 0;
+    if(ratingWinner === 0) return ratingOpponent;
 
-    if(ratingWinner === 0){
-        result = ratingOpponent;
+    if(ratingOpponent === 0) return ratingWinner
+
+    if (ratingOpponent > ratingWinner) {
+        return Number((ratingWinner + (ratingOpponent - ratingWinner + 5) / 3).toFixed(1));
     }
 
-    else if(ratingOpponent === 0){
-        result = ratingWinner;
-    }
-
-    else if(ratingWinner >= ratingOpponent){
-        const difference = ratingWinner - ratingOpponent;
-        if (difference >=0  && difference <=2){
-            bonus = 2;
-        }
-        else if(difference > 2 && difference < 20){
-            bonus = 1;
-        }
-        else if(difference >= 20){
-            bonus = 0;
-        }
-        result = ratingWinner + bonus;
-    }
-
-    else {
-        bonus = (ratingOpponent - ratingWinner +5)/3;
-        result = ratingWinner + bonus
-    }
-
-
-    return Number(result.toFixed(1));
+    const difference = ratingWinner - ratingOpponent;
+    if (difference <= 2) return ratingWinner + 2;
+    if (difference < 20) return ratingWinner + 1;
+    
+    return ratingWinner;
 }   
 
+console.log(calculateRating(10, 12));
